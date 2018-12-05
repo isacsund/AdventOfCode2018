8a8172da58283b785638a023a73995d624bde0a1
Isac Sund <isac.sund@outlook.com>
2018-12-06 00:49:43 +0200

    Dont need mkString and split

--- src/main/scala/adventofcode/Day01.scala
+++ src/main/scala/adventofcode/Day01.scala
@@ -1,6 +1,5 @@
 package adventofcode
 
-import adventofcode.Day01.{readInput}
 
 import scala.collection.mutable.HashSet
 import scala.io.Source
@@ -10,24 +9,25 @@ object Day01 extends App {
   PartOne
   PartTwo
 
-  def readInput(): String = Source.fromFile(s"input/01.txt").getLines().mkString("\n")
-}
 
-object PartOne {
-  val frequency = readInput().split("\n").map(_.toInt).sum
-  println(s"Frequency: $frequency")
-}
-
-object PartTwo {
-  var frequency = 0
-  var frequencies = new HashSet[Int]()
-  var idx = 0
-  val deltaFrequency = readInput().split("\n").map(_.toInt)
-
-  while (frequencies.add(frequency)) {
-    frequency += deltaFrequency(idx % deltaFrequency.length)
-    idx += 1
+  object PartOne {
+    val frequency = readInput().map(_.toInt).sum
+    println(s"Frequency: $frequency")
   }
-  println(s"Duplicate: $frequency")
 
+  object PartTwo {
+    var frequency = 0
+    var frequencies = new HashSet[Int]()
+    var idx = 0
+    val deltaFrequency = readInput().map(_.toInt)
+
+    while (frequencies.add(frequency)) {
+      frequency += deltaFrequency(idx % deltaFrequency.length)
+      idx += 1
+    }
+    println(s"Duplicate: $frequency")
+
+  }
+
+  def readInput(): Vector[String] = Source.fromFile(s"input/01.txt").getLines().toVector
 }
